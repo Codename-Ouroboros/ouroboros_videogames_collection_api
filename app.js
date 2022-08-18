f02786510f7beac72d7c17313f262dd8be0b5405
Guillermo Granados Gómez <pytonicus@gmail.com>
2022-08-18 08:38:00 +0200

    setup database connect and json parser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-
 // Import routes:
 const companiesRoutes = require('./routes/companiesRoutes.js');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Route list:
 app.use('/api', companiesRoutes);
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const { default: mongoose } = require('mongoose');
 const app = require('./app');
 const port = 3000;
 const host = "http://localhost"
+
+mongoose.connect("mongodb://localhost:27017/game_collector", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    try{
+        if(err){
+            throw err;
+        }else{
+            console.log("Successful database connection")
+        }
+    }catch(error){
+        console.error(error);
+    }
+});
 
 app.listen(port, () =>{
     console.log(`Server works in: ${host}:${port}`);
